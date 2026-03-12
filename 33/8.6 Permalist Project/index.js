338e20d95d51a8376192ad80aceb792e6bc72f18
StebaneJhon <111849849+StebaneJhon@users.noreply.github.com>
2026-03-13 00:00:27 +0100

    33

---
+++ 33/8.6 Permalist Project/index.js
@@ -0,0 +1,65 @@
+import express from "express";
+import bodyParser from "body-parser";
+import pg from "pg";
+
+const app = express();
+const port = 3000;
+
+const db = new pg.Client({
+  user: "postgres",
+  host: "localhost",
+  database: "permalist",
+  password: "bajgon-pyGno9-sasjip",
+  port: 5432,
+});
+db.connect();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let items = [
+  { id: 1, title: "Buy milk" },
+  { id: 2, title: "Finish homework" },
+];
+
+async function setItems() {
+  const result = await db.query("SELECT * FROM items ORDER BY id ASC;");
+  items = result.rows;
+};
+
+app.get("/", async (req, res) => {
+  await setItems();
+  res.render("index.ejs", {
+    listTitle: "Today",
+    listItems: items,
+  });
+});
+
+app.post("/add", async (req, res) => {
+  const item = req.body.newItem;
+  await db.query(
+    "INSERT INTO items (title) VALUES ($1);",
+    [item.trim()]
+  )
+  res.redirect("/");
+});
+
+app.post("/edit", async (req, res) => {
+  const itemId = req.body.updatedItemId;
+  const itemTitle = req.body.updatedItemTitle;
+  await db.query(
+    "UPDATE items SET title = $1 WHERE id = $2;",
+    [itemTitle.trim(), itemId]
+  )
+  res.redirect("/")
+});
+
+app.post("/delete", async (req, res) => {
+  const itemId = req.body.deleteItemId;
+  await db.query("DELETE FROM items WHERE id = $1;", [itemId])
+  res.redirect("/")
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
